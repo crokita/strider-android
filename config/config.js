4bf016e4da7817226e9ddb17a9d12bbf514b70e9
Chris Rokita <chris.rokita@livioradio.com>
2015-01-30 15:31:09 -0500

    testing multiple routes

--- config/config.js
+++ config/config.js
@@ -25,12 +25,32 @@ app.controller('AndroidController', ['$scope', '$http', function ($scope, $http)
 
 	$scope.retrieveData = function () {
 		$scope.save();
-		/*$http.get('/ext/android/devices').success(function(data, status, headers, config) {
+		$http.get('/ext/android/devices').success(function(data, status, headers, config) {
 			console.log(data);
 			console.log(config);
 			$scope.result = data;
-		});*/
-		$http.get('api/android/devices').success(function(data, status, headers, config) {
+		});
+		$http.get('/api/android/devices').success(function(data, status, headers, config) {
+			console.log(data);
+			console.log(config);
+			$scope.result = data;
+		});
+		$http.get('/crokita/auto_dummy/api/android/devices').success(function(data, status, headers, config) {
+			console.log(data);
+			console.log(config);
+			$scope.result = data;
+		});
+		$http.get('/crokita/auto_dummy/api/strider-android/devices').success(function(data, status, headers, config) {
+			console.log(data);
+			console.log(config);
+			$scope.result = data;
+		});
+		$http.get('/crokita/auto_dummy/api/android').success(function(data, status, headers, config) {
+			console.log(data);
+			console.log(config);
+			$scope.result = data;
+		});
+		$http.get('/crokita/auto_dummy/api/strider-android').success(function(data, status, headers, config) {
 			console.log(data);
 			console.log(config);
 			$scope.result = data;
