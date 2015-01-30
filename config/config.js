b6e3cc93eb3be854c62232ad61162ebb3cad2a34
Chris Rokita <chris.rokita@livioradio.com>
2015-01-30 14:29:30 -0500

    testing

--- config/config.js
+++ config/config.js
@@ -27,8 +27,6 @@ app.controller('AndroidController', ['$scope', '$http', function ($scope, $http)
 		$scope.save();
 		$http.get('/crokita/auto_dummy/config/plugin-android/devices').success(function(data, status, headers, config) {
 			console.log(data);
-			console.log(status);
-			console.log(headers);
 			console.log(config);
 			$scope.result = data;
 		});
