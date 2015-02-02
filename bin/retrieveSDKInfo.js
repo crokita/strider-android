f8a6ccdba71e8d341baea5be3e99b9dc7a3702e0
Chris Rokita <chris.rokita@livioradio.com>
2015-02-02 10:09:16 -0500

    testing

--- bin/retrieveSDKInfo.js
+++ bin/retrieveSDKInfo.js
@@ -40,6 +40,42 @@ var parseDeviceList = function (input) {
 	return deviceList;
 }
 
+parseTargetList = function (input) {
+	var list = input.match(/Name: .*|Type: .*|Tag\/ABIs : .*/g);
+	var groupedList = [];
+	for (match in list) {
+		var threeProperties = list.splice(0,3);
+		groupedList.push(threeProperties);
+	}
+	var result = "";
+
+	var success = groupedList.every(function (element, index, array) {
+		var name = element[0].replace("Name: ", "");
+		var type = element[1].replace("Type : ", "");
+		var abis = element[2].replace("Tag/ABIs :", "");
+		//console.log("Name: " + name);
+		//console.log("Type: " + type);
+		//console.log("ABIs: " + abis);
+		if (abis == "no ABIs") {
+			if (type == "Platform") { //a platform which has no ABIs cannot run on an emulator. error out
+				return false;
+			}
+		}
+		result += name + "\n"
+		return true;
+	});
+
+	if (success) {
+		//return the list of android targets
+		return result;
+	}
+	else {
+		//don't return anything
+	}
+
+}
+
+/*
 //this function takes the list of android targets that are usuable and returns a list of them
 var parseTargetList = function (input) {
 	var targetList = [];
@@ -49,5 +85,6 @@ var parseTargetList = function (input) {
 
 	
 
-	return deviceList;
+	return targetList;
 }
+*/
