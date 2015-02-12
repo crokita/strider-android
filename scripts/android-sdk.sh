9201023a777b5886feff70a6ca61a8e4d5c37d24
Chris Rokita <chris.rokita@livioradio.com>
2015-02-12 10:53:40 -0500

    update gitignore

--- scripts/android-sdk.sh
+++
@@ -1,3 +0,0 @@
-#download the android SDK automatically
-sudo apt-get --force-yes --yes install expect #install expect to auto say yes to android license agreements.
-./get-sdk.sh #check if the sdk is installed, and if not, install it
