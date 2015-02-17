65ff52ac628e5cfb651a03a3ccb4952b3d782a4d
Chris Rokita <chris.rokita@livioradio.com>
2015-02-17 10:30:01 -0500

    Remove the skin configuration. It won't matter for CI anyway

--- bin/retrieveSDKInfo.js
+++ bin/retrieveSDKInfo.js
@@ -79,14 +79,14 @@ module.exports = {
 	}
 }
 
-//this function takes the list of android devices that are usuable and converts each name, target, abi and skin to an object and returns a list
+//this function takes the list of android devices that are usuable and converts each name, target, abi to an object and returns a list
 var parseDeviceList = function (input) {
 	var deviceList = [];
 	var list = input.split("\n");
 	//remove the first line
 	list.splice(0,1);
 
-	for (var index = 0; index < list.length; index += 5) {
+	for (var index = 0; index < list.length; index += 6) {
 		var deviceObj = {
 			name: list[index].replace("Name:", "").trim(),
 			target: list[index + 2].replace("Target:", "").trim(),
