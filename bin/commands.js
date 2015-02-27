8a292c8b22ae02aa9ddff809acacd782b05d92c5
Chris Rokita <chris.rokita@livioradio.com>
2015-02-27 10:19:26 -0500

    Restructure the project

--- bin/commands.js
+++ bin/commands.js
@@ -1,6 +1,7 @@
 //all the dirty work goes here
 //used for easily creating commands
 var exec = require('child_process').exec;
+var fs = require('fs');
 
 var defaultSDKLocation  =   'android-sdk-linux';
 var androidDir 			= 	'/tools/android';
@@ -14,23 +15,24 @@ module.exports = {
 		} 
 
 		//go to the directory of the SDK
-		goToAndroid(location, function () {
-			exec('./android list avd', function (err, stdout, stderr) {
-				console.log(err);
-				console.log(stdout);
-		        return callback(stdout);
-		    });
-		});
+		goToAndroid(location);
+		console.log(process.cwd());
+		
+		exec('./android list avd', function (err, stdout, stderr) {
+			console.log(err);
+			console.log(stdout);
+	        return callback(stdout);
+	    });
 	}
 
 }
 
 //goes to the tools folder of the SDK given a location. also gives permission to execute the android tool
-var goToAndroid = function (location, callback) {
+var goToAndroid = function (location) {
 	process.chdir(process.env.HOME);
 	process.chdir(location);
+	process.chdir("tools");
+	fs.chmodSync('android', '755');
 
-	exec('chmod 755 tools/android; cd tools', function (err, stdout, stderr) {
-        return callback();
-    });
+	return;
 }
