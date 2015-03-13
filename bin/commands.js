924453c2df1cfd5016906bab7f306ad0cedc8383
Chris Rokita <chris.rokita@livioradio.com>
2015-03-13 10:57:47 -0400

    Fix the emulator booting and killing

--- bin/commands.js
+++ bin/commands.js
@@ -89,6 +89,7 @@ module.exports = {
 
 		//var emulatorCommand = child.spawn(emulator, ["-avd", deviceName, "-no-skin", "no-audio", "no-window", "-no-boot-anim"]);
 		//workers.push(emulatorCommand);
+		console.log(adb);
 		var adbCommand = child.spawn(adb, ["wait-for-device"]);
 		workers.push(adbCommand);
 
