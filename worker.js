2149fb871e64c44fd1ddffc326a520ff0c77d639
Chris Rokita <chris.rokita@livioradio.com>
2015-03-24 14:37:28 -0400

    Make Strider fail if tests fail

--- worker.js
+++ worker.js
@@ -51,7 +51,6 @@ module.exports = {
 				context.logger.trace("error message");
 				context.logger.assert(true); //just make this true
 */
-/*
 				SDK.findEmulator(context, function (result) {
 					if (!result) { //if it didn't return a matching emulator then start a new one
 						context.out("No emulator found. Starting up emulator\n");
@@ -62,8 +61,8 @@ module.exports = {
 					else {
 						context.out("Found running emulator: " + result + "\n");
 					}
-				});*/
-				done(null, true);
+				});
+				
 			},
 			//function style (calling done is a MUST)
 			test: function (context, done) {
@@ -75,12 +74,11 @@ module.exports = {
 					ide: config.ide,
 					sdkLocation: config.sdkLocation
 				};
-/*
+
 				SDK.installApk(configData, context, function (err, result) {
 					done(err, result); //result is a boolean. err is an error message (if any)
 					//if there is anything in err then Strider will recognize the test as a failure
-				});*/
-				done(null, true);
+				});
 			}
 			//deploy: '',
 			//cleanup: ''
