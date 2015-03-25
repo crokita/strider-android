5bf9b20030bea85b58ea772918641deb1d53ea38
Chris Rokita <chris.rokita@livioradio.com>
2015-03-25 11:54:06 -0400

    Make Strider fail if tests fail

--- README.md
+++ README.md
@@ -33,5 +33,4 @@ TODO LIST:
 *  Allow the user to select a subset of tests to perform
 *  Support for IntelliJ IDEA projects?
 *  Remove the apks from the emulator when it's finished?
-*  Change the search from checking if a failure happens to changing if a test passes? (use the regex "OK \(\d* test(s*)\)")
 *  Option to automatically set up the Android SDK?
--- bin/commands.js
+++ bin/commands.js
@@ -518,13 +518,12 @@ var runTheTests = function(context, decoder, path) {
 		runTestsCmd.on('close', function (code) {
 			//Finding "FAILURES!!!" means the tests have failed. In any other case make it a failed test
 			//check whether the unit tests have passed
-			var result = fullOutputResults.search(/FAILURES!!!/g);
+			//var result = fullOutputResults.search(/FAILURES!!!/g);
+			var result = fullOutputResults.search(/OK \(\d* test(s*)\)/g);
 			if (result == -1) {
-				console.log("THE TEST PASSED!");
-				return next(null, true);
+				return next(null, true); //the tests passed
 			}
 			else {
-				console.log("THE TEST FAILED!");
 				return next(1, true); //non-zero number will cause a failure
 			}
 			
