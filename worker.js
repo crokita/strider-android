17bc82f363ee3f57d3c9eba2780581ff4c6e4f67
Chris Rokita <chris.rokita@livioradio.com>
2015-03-25 13:50:38 -0400

    Fix the duplicate phase error

--- worker.js
+++ worker.js
@@ -10,7 +10,7 @@ module.exports = {
 	init: function (config, job, context, cb) {
 		//config = config || {};
 		//console.log(config);
-		
+		/*
 		return cb(null, {
 			listen: function (emitter, context) {
 				emitter.on('job.status.phase.done', function (id, data) {
@@ -20,7 +20,7 @@ module.exports = {
 				});
 			}
 		});
-/*
+*/
 		return cb(null, {
 			// any extra env variables. Will be available during all phases
 			env: {},
@@ -55,7 +55,7 @@ module.exports = {
 				SDK.findEmulator(context, function (result) {
 					if (!result) { //if it didn't return a matching emulator then start a new one
 						context.out("No emulator found. Starting up emulator\n");
-						SDK.startEmulator(configData, context, function (err, result) {
+						SDK.startEmulator(configData, context, function (code) {
 							done(null, true);
 						});
 					}
@@ -84,7 +84,7 @@ module.exports = {
 			//deploy: '',
 			//cleanup: ''
 
-		});//TODO: uninstall the apks in the cleanup phase?*/
+		});//TODO: uninstall the apks in the cleanup phase?
 	},
 	// this is only used if there is _no_ plugin configuration for a
 	// project. See gumshoe for documentation on detection rules.
