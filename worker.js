6968d71d68f816e7cb5764d0eddfd30157ee1595
Chris Rokita <chris.rokita@livioradio.com>
2015-03-26 11:05:56 -0400

    Kill running emulators

--- worker.js
+++ worker.js
@@ -44,7 +44,7 @@ module.exports = {
 
 				SDK.findEmulator(context, function (result) {
 					if (!result) { //if it didn't return a matching emulator then start a new one
-						context.out("No emulator found. Starting up emulator\n");
+						context.out("No emulator found. Starting up emulator " + result + "\n");
 						SDK.startEmulator(configData, context, function (code) {
 							done(null, true);
 						});
