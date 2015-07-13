f4ea7b587592a65472bec9305e2d42d976670609
Chris Rokita <chris.rokita@livioradio.com>
2015-07-13 15:04:26 -0400

    Fix emulator errors

--- bin/commands.js
+++ bin/commands.js
@@ -133,9 +133,7 @@ module.exports = {
 		var emulatorCommand;
 		var adbCommand;
 
-		console.log("SDK LOCATION");
-		console.log(sdkLocation);
-		if (sdkLocation != "") { //assume android tool is in the path if no location is specified
+		if (sdkLocation == "") { //assume android tool is in the path if no location is specified
 			emulatorCommand = child.spawn("emulator", ["-avd", deviceName, "-no-skin", "-no-audio", "-no-window", "-no-boot-anim"]);
 			//workers.push(emulatorCommand);
 			adbCommand = child.spawn("adb", ["wait-for-device"]);
