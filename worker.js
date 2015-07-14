97834493cf240de245596f52917c60f6fbbf3cfa
Chris Rokita <chris.rokita@livioradio.com>
2015-07-14 16:38:15 -0400

    Add javadoc

--- worker.js
+++ worker.js
@@ -38,28 +38,55 @@ module.exports = {
 					isLibrary: config.isLibrary,
 					testFolderName: config.testFolderName,
 					ide: config.ide,
-					sdkLocation: config.sdkLocation
+					sdkLocation: config.sdkLocation,
+					javadocs: config.javadocs
 				};
 				//context.comment("This is a comment. It gets shown on the Strider webpage");
 
-				SDK.findEmulator(context, function (result) {
-					if (!result) { //if it didn't return a matching emulator then start a new one
-						context.out("No emulator found. Starting up emulator " + configData.device + "\n");
-						SDK.startEmulator(configData, context, function (code) {
-							done(null, true);
+				var tasks = [];
+
+				if (configData.javadocs) { //user wants documentation generation
+					tasks.push( 
+						function (next) {
+							SDK.generateJavaDocs(configData, context, function (err, result) {
+								if (!result) {
+									next(null);
+								}
+								else {
+									next(null);
+								}
+							});
+						}
+					);
+				}
+
+				tasks.push( 
+					function (next) {
+						SDK.findEmulator(context, function (result) {
+							if (!result) { //if it didn't return a matching emulator then start a new one
+								context.out("No emulator found. Starting up emulator " + configData.device + "\n");
+								SDK.startEmulator(configData, context, function (code) {
+									next(null, true);
+								});
+							}
+							else {
+								context.out("Found running emulator: " + result + "\n");
+								//if the found emulator is the same as the emulator we want to run then leave it alone
+								//if the found emulator is different then kill it and start the proper one
+								//if (result == config.device) { TODO: fix this. config.device and the emulator name that returns aren't the same name
+									next(null, true);
+								//}
+								//else {
+									
+								//}
+							}
 						});
 					}
-					else {
-						context.out("Found running emulator: " + result + "\n");
-						//if the found emulator is the same as the emulator we want to run then leave it alone
-						//if the found emulator is different then kill it and start the proper one
-						//if (result == config.device) { TODO: fix this. config.device and the emulator name that returns aren't the same name
-							done(null, true);
-						//}
-						//else {
-							
-						//}
-					}
+				);
+
+				//execute the task(s) added above
+				async.waterfall(tasks, function (err, result) {
+					callback(err, result);
 				});
 				
 			},
